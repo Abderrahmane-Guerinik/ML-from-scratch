1e7978b687c1ff300f714849ac986e2abbba2e81
Guerinik Abderrahmane <ca.guerinik@esi-sba.dz>
2026-01-05 18:43:28 +0100

    initialize linear regression

--- mlfs/linear_regression.py
+++ mlfs/linear_regression.py
@@ -1,6 +1,19 @@
-class LinearRegression():
+import numpy as np
+
+
+def mse(y, y_hat):
+    mse = 1/(2 * y.size) * np.sum((y_hat - y) ** 2)
+    return mse
+
+
+class LinearRegression:
+    def __init__(self):
+        self.w = 0
+        self.b = 0
+
     def fit(X, y):
         return None
 
-    def predict(X):
-        return None
+    def predict(self, x):
+        y_hat = self.w * x + self.b
+        return y_hat
