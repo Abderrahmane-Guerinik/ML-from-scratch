e5754a93cc8931ee06062e7613963a1120502e75
Guerinik Abderrahmane <ca.guerinik@esi-sba.dz>
2026-01-06 20:37:04 +0100

    add simple linear regression with GD

--- mlfs/linear_regression.py
+++ mlfs/linear_regression.py
@@ -8,10 +8,20 @@ def mse(y, y_hat):
 
 class LinearRegression:
     def __init__(self):
-        self.w = 0
-        self.b = 0
+        # initialize w and b with random values
+        self.w = np.random.random(size=1)
+        self.b = np.random.random(size=1)
 
-    def fit(X, y):
+    def fit(self, X, y, alpha=0.01, epochs=50):
+        # apply gradient descent as an optimizer to train the model
+        for i in range(epochs):
+            # simultanous update of the parameters w and b
+            m = len(X)
+            tmp_w = self.w - alpha * \
+                (1/m) * np.sum(((self.w * X + self.b) - y) * X)
+            tmp_b = self.b - alpha * 1/m * np.sum((self.w * X + self.b) - y)
+            self.w = tmp_w
+            self.b = tmp_b
         return None
 
     def predict(self, x):
